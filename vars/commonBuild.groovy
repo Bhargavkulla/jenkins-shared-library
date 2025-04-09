ba5a309b88864fdd82fbbc09a4b533a5e2180b92
Bhargavkulla <118447823+Bhargavkulla@users.noreply.github.com>
2025-04-09 17:31:31 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -1,15 +1,9 @@
 def call() {
     stage('Checkout') {
-        checkout scm
+        git url: 'https://github.com/YOUR-REPO/sample-app.git', branch: 'main'
     }
-
     stage('Unit Tests') {
         echo 'Running unit tests...'
         sh './gradlew test'
-    }
-
-    stage('Deploy') {
-        echo "Deploying to ${env.ENVIRONMENT} environment..."
-        // You can add deployment logic here
     }
 }
