d6960cacc06dedd71fe7c4f6f0cc5c478f75be1e
Bhargavkulla <118447823+Bhargavkulla@users.noreply.github.com>
2025-04-09 17:32:32 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -1,6 +1,6 @@
 def call() {
     stage('Checkout') {
-        git url: 'https://github.com/YOUR-REPO/sample-app.git', branch: 'main'
+        git url: 'https://github.com/Bhargavkulla/jenkins-shared-library.git', branch: 'main'
     }
     stage('Unit Tests') {
         echo 'Running unit tests...'
